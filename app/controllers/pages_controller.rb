77092605df9d4da0e5abe2275f971ded5ece5316
Matthieu Lombard <matthieu.lombard@hotmail.be>
2018-05-01 13:49:49 +0200

    img

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :proba]
 
   def home
+    @proba=rand(10)
   end
+
 end
